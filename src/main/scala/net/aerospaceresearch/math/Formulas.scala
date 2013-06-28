1956c4ad41082748da14a56e417cbea9253a4ded
Elmar Athmer <elmar@athmer.org>
2013-06-28 19:09:17 +0200

    add formula for acceleration

--- src/main/scala/net/aerospaceresearch/math/Formulas.scala
+++ src/main/scala/net/aerospaceresearch/math/Formulas.scala
@@ -37,4 +37,6 @@ object Formulas {
   def gravitationalForces(a: Body)(b: Body): DenseVector[Double] =
     (b.r - a.r) * (G * a.mass * b.mass) * (1 / BigDecimal(norm(b.r - a.r)).pow(3).toDouble)
 
+  def acceleration(a: Body)(bodies: List[Body]) =
+    bodies.map(that => (that.r - a.r) * (G * that.mass) * (1 / BigDecimal(norm(that.r - a.r)).pow(3).toDouble))
 }
