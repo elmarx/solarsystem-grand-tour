41c92d925e5f64212d614b4389f223b8f92a3f30
Elmar Athmer <elmar@athmer.org>
2013-07-11 14:27:11 +0200

    update copyright

--- src/test/scala/net/aerospaceresearch/jplparser/PlanetSuite.scala
+++ src/test/scala/net/aerospaceresearch/jplparser/PlanetSuite.scala
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2013 Elmar Athmer
+ *
+ * This file is part of SolarSystemGrandTour.
+ *
+ * SolarSystemGrandTour is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SolarSystemGrandTour is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SolarSystemGrandTour.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.aerospaceresearch.jplparser
 
 import org.scalatest.FunSuite
 
-/**
- * Created by elmar on 06.07.13.
- */
 class PlanetSuite extends FunSuite {
 
   test("planet ids match item numbering in header") {
