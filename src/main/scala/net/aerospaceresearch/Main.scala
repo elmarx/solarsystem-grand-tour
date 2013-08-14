b272f919fd62b2b07e85e1b225837128cdf204a1
Elmar Athmer <elmar@athmer.org>
2013-08-14 23:44:17 +0200

    some more crazy csv output

--- src/main/scala/net/aerospaceresearch/Main.scala
+++ src/main/scala/net/aerospaceresearch/Main.scala
@@ -21,8 +21,7 @@ package net.aerospaceresearch
 
 import net.aerospaceresearch.jplparser.DataReader
 import breeze.linalg.DenseVector
-import org.joda.time.DateTime
-import scala.collection.parallel.immutable.ParSeq
+import org.joda.time.{DateTimeUtils, DateTime}
 import net.aerospaceresearch.model.Body
 
 
@@ -35,25 +34,43 @@ import net.aerospaceresearch.model.Body
 object Main {
 
   def main(args: Array[String]) {
-    val startedAt = DateTime.now.toInstant.getMillis
+    val now: Double = DateTimeUtils.toJulianDay(DateTime.now.toInstant.getMillis)
+    val systemNow = new DataReader(now).system
+    val systemPlusOne = new DataReader(now + 1).system
 
-    val system = DataReader.current().system
-    val velocities = system.velocities
-    val positions = system.positions
+    val velocities = systemNow.velocities
+    val positions = systemNow.positions
+
+    println("Starting at julian time " + systemNow.time)
 
 
-    val header = List("Planet", "vX", "vY", "vZ", "rX", "rY", "rZ")
+    val header = List("Planet",
+      "v0X (Ephimeriden)", "v0Y (Ephimeriden)", "v0Z (Ephimeriden)",
+      "v1X (Ephimeriden)", "v1Y (Ephimeriden)", "v1Z (Ephimeriden)",
+      "v1X (berechnet)", "v1Y (berechnet)", "v1Z (berechnet)",
+      "δv1X", "δv1Y", "δv1Z",
+      "r0X (Ephimeriden)", "r0Y (Ephimeriden)", "r0Z (Ephimeriden)",
+      "r1X (Ephimeriden)", "r1Y (Ephimeriden)", "r1Z (Ephimeriden)",
+      "r1X (berechnet)", "r1Y (berechnet)", "r1Z (berechnet)",
+      "δr1X", "δr1Y", "δr1Z"
+    )
 
     println(header.mkString(";"))
 
-    val data: List[(Body, (DenseVector[Double], DenseVector[Double]))] = system.bodies.zip(velocities.zip(positions))
-    data.map { case (a, (v, r)) =>
-      List(a.identity.toString, v(0), v(1), v(2), r(0), r(1), r(2)).mkString(";")
+    val data: List[((Body, (DenseVector[Double], DenseVector[Double])), Body)] =
+      systemNow.bodies.zip(velocities.zip(positions)).zip(systemPlusOne.bodies)
+
+    data.map { case ((a0, (v1, r1)), a1) =>
+      List(a0.identity.toString,
+        a0.v0(0), a0.v0(1), a0.v0(2),
+        a1.v0(0), a1.v0(1), a1.v0(2),
+        v1(0), v1(1), v1(2),
+        v1(0) - a1.v0(0), v1(1) - a1.v0(1), v1(2) - a1.v0(2),
+        a0.r0(0), a0.r0(1), a0.r0(2),
+        a1.r0(0), a1.r0(1), a1.r0(2),
+        r1(0), r1(1), r1(2),
+        r1(0) - a1.r0(0), r1(1) - a1.r0(1), r1(2) - a1.r0(2)
+      ).mkString(";")
     }.foreach(println)
-
-
-    val time = DateTime.now.toInstant.getMillis - startedAt
-
-    println("This task ran " + time + " milliseconds")
   }
 }
