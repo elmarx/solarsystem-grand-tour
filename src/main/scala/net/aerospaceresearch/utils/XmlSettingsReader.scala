043248778edc4336ff9742ba12d15a971d9b2eea
Elmar Athmer <elmar@athmer.org>
2013-08-18 15:37:56 +0200

    implement a reader for xml input settings

---
+++ src/main/scala/net/aerospaceresearch/utils/XmlSettingsReader.scala
@@ -0,0 +1,42 @@
+package net.aerospaceresearch.utils
+
+import scala.xml.{Node, XML}
+import net.aerospaceresearch.model.{Probe, Body}
+import breeze.linalg.DenseVector
+
+/**
+ *
+ * Created by elmar on 18.08.13.
+ */
+class XmlSettingsReader(file: String) {
+
+  val ssgt = XML.loadFile(file)
+  val settings = ssgt \ "settings"
+
+  val startTime = (settings \ "startTime").text.toDouble
+  val days = (settings \ "days" ).text.toInt
+  val outputDir = (settings \ "outputDir" ).text
+
+  val bodies = (ssgt \ "bodies")(0).child.map(xmlNodeToBody)
+
+  def xmlNodeToBody(e: Node): Body = {
+    val name = (e \ "name").text
+    val mass = (e \ "mass").text.toDouble
+    val r = DenseVector[Double](
+      (e \ "r" \ "@x").text.toDouble,
+      (e \ "r" \ "@y").text.toDouble,
+      (e \ "r" \ "@z").text.toDouble
+    )
+    val v = DenseVector[Double](
+      (e \ "v" \ "@x").text.toDouble,
+      (e \ "v" \ "@y").text.toDouble,
+      (e \ "v" \ "@z").text.toDouble
+    )
+
+    e.label match {
+      case "probe" => Probe(name, mass, r, v)
+      case x => throw new Exception("Don't know how to handle node of type " + x)
+    }
+  }
+
+}
